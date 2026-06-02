3ab6b114c23b36434de0e92a4840b99ec89d2420
Markush2005 <haatmarkus@gmail.com>
2026-06-02 07:57:08 +0200

    Update einfaches zahlenratespiel.py

--- einfaches zahlenratespiel.py
+++ einfaches zahlenratespiel.py
@@ -1,7 +1,7 @@
 geheime_zahlt = 1
 zahl=int(input("Errate die geheime Zahl zwischen 1 und 10\n "))
 if zahl == geheime_zahlt:
-    print("Glückwunsch du hast die zahlt erraten.")
+    print("Glückwunsch du hast die Zahl erraten.")
 else:
-    print(f"Falsch die Richtige zahl war {geheime_zahlt}.")
+    print(f"Falsch die richtige Zahl war {geheime_zahlt}.")
 input("Zum schlie0en Enter drücken")
